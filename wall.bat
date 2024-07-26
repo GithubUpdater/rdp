39e003340f3a49f95096fab0d09557edbedde6d3
GithubUpdater <152162409+GithubUpdater@users.noreply.github.com>
2024-07-27 06:00:28 +0800

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,14 +1,14 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GithubUpdater/rdp/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GithubUpdater/rdp/main/TranscodedWallpaper.jpeg -OutFile TranscodedWallpaper.jpeg"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GithubUpdater/rdp/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
 
-set "TranscodedWallpaper=TranscodedWallpaper"
+set "TranscodedWallpaper=TranscodedWallpaper.jpeg"
 set "CachedImage=CachedImage_1024_768_POS4.jpg"
 
 set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
 set "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
 
-copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
+copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper.jpeg"
 copy /y "%CachedImage%" "%cachedFileDir%\CachedImage_1024_768_POS4.jpg"
 
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
